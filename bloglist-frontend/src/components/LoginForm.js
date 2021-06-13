e29c64ffb00aa3e2c536eb81c97513f54cbc997d
Joe <joedravarol@gmail.com>
2021-06-13 12:11:02 +0100

    Complete bloglist frontend exercise step1 feat: Implement login functionality

---
+++ bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const loginForm = ({ 
+  username,
+  password,
+  handleLogin,
+  handleUsernameChange,
+  handlePasswordChange
+}) => {
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>Log in to application</h2>
+      
+      <div>
+        username
+        <input
+          type='text'
+          value={username}
+          name='Username'
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type='password'
+          value={password}
+          name='Password'
+          onChange={handlePasswordChange}
+        />
+      </div>
+
+      <button type='submit'>login</button>
+    </form>
+  )
+}
+
+export default loginForm
