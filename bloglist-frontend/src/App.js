8ad33e659327f81ae0e7a4867fe9448f21761899
Joe <joedravarol@gmail.com>
2021-06-14 20:48:59 +0100

    Complete blog list frontend exercise step9 feat: Modify the application to list the blog posts by the number of likes.

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -102,6 +102,9 @@ const App = () => {
 
   const blogFormRef = useRef()
 
+  // Sort in descending order
+  const sortedBlogsByLikes = blogs.sort((a,b) => b.likes - a.likes)
+
   if (user === null) {
     return (
       <>
@@ -133,7 +136,7 @@ const App = () => {
         <BlogForm createBlog={addBlog} />
       </Togglable>
 
-      {blogs.map(blog =>
+      {sortedBlogsByLikes.map(blog =>
         <Blog 
           key={blog.id}
           blog={blog}
