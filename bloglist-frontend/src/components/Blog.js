75f7aa91906ce1b55efd6440c32e44448fedf41d
Joe <joedravarol@gmail.com>
2021-06-14 13:28:26 +0100

    Complete blog list frontend exercise step7 feat: Add a button to each blog, which controls whether all of the details abuot the blog are shown or not.

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,41 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <div>
+          likes {blog.likes}
+          <button>like</button>
+        </div>
+        <p>{blog.author}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
